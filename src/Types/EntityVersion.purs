c43d31b466f329884e8836c2283b3cfdf750e832
Dennis Snell <dennis.snell@automattic.com>
2017-03-26 00:16:01 -0500

    Add EntityVersion and tests

---
+++ src/Types/EntityVersion.purs
@@ -0,0 +1,27 @@
+module Types.EntityVersion
+  ( EntityVersion
+  , entityVersion
+  , isValidEntityVersion
+  ) where
+
+import Prelude (class Eq, class Ord, class Show, compare, map, show, (==), (<<<))
+import Data.Maybe (Maybe, isJust)
+
+newtype EntityVersion = EntityVersion Int
+
+entityVersion :: Int -> Maybe EntityVersion
+entityVersion n = (map EntityVersion) <<< maybeVersion
+  where
+    maybeVersion = if n >= 0 then Just n else Nothing
+
+isValidEntityVersion :: Int -> Boolean
+isValidEntityVersion = isJust <<< entityVersion
+
+instance eqEntityVersion :: Eq EntityVersion where
+  eq (EntityVersion a) (EntityVersion b) = a == b
+
+instance ordEntityVersion :: Ord EntityVersion where
+  compare (EntityVersion a) (EntityVersion b) = compare a b
+
+instance showEntityVersion :: Show EntityVersion where
+  show (EntityVersion a) = show a
